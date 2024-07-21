deb25edfde318d6342d3b9fd50996cf204622e68
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-21 08:27:01 +0000

    Third Challenge Complete

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -4,13 +4,27 @@ export default function FormValidator () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password && passwordConfirm) {
+      if ((password.length >= 8) && (passwordConfirm.length) >= 8 && (password === passwordConfirm)) {
+        setMessage("User Created!")
+      } else {
+        setMessage("Password Error")
+      }
+    } else {
+      setMessage("All the fields in the form are to be filled.");
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
-        type='text' name='email'
+        type='email' name='email'
         onChange={e => setEmail(e.target.value)}
       />
       <label htmlFor='password'>Password</label>
@@ -23,6 +37,7 @@ export default function FormValidator () {
         type='password' name='password-confirm'
         onChange={e => setPasswordConfirm(e.target.value)}
       />
+      <p>{message}</p>
       <input type='submit' value='Submit' />
     </form>
   )
