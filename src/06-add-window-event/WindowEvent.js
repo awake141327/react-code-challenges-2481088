98ef5c70d8835cc4621573937691e316a48dd1b6
Amaan <82905413+awake141327@users.noreply.github.com>
2024-07-21 10:51:01 +0000

    Sixth Challenge Complete

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,14 @@
+import { useEffect} from 'react';
+
 export default function WindowEvent () {
+
+  useEffect(() => {
+    const handleDblClick = () => alert("Mouse Pressed");
+    window.addEventListener("dblclick", handleDblClick);
+
+    return () => window.removeEventListener("dblClick", handleDblClick);
+  }, [])
+
   return (
     <h2>Window event active</h2>
   )
